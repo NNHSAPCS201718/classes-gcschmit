3a535d8601e6d9bab009c3c5416380985109fa6b
Geoff Schmit <gcschmit@gmail.com>
2017-09-11 12:38:40 -0500

    started chapter 3 notes
    start with defining the addGas method of the Car class next time

---
+++ Class Notes/Car.java
@@ -0,0 +1,51 @@
+
+/**
+ * This class models a car.
+ *
+ * @author gcschmit
+ * @version 11 September 2017
+ */
+public class Car
+{
+    /*
+     * Instance variables store the object's properties
+     *      specify the visibility (e.g., private)
+     *          public: accessible by anyone
+     *          private: only accessible by methods in this class
+     *      specify the type (e.g., double)
+     *      specify the name (e.g., fuelEfficiency, fuelInTank)
+     *      
+     *  Instance variables differ from local variables in the following ways:
+     *      scoped to their class (accessible for all methods)
+     *      automatically initialized to a default value (0, false, null)
+     */
+    private double fuelEfficiency; // in units of miles per gallon
+    private double fuelInTank; // in units of gallons
+    
+    
+    /*
+     * When defining a method, specify:
+     *      the visibilty (e.g., public)
+     *      the return type (e.g., void)
+     *      the method name (e.g., drive)
+     *      the parameters and their types (e.g., distance of type double)
+     */
+    
+    /**
+     * Drives this car the specified distance, consuming fuel
+     * 
+     * @param distance the distance, in miles, this car drives
+     */
+    public void drive( double distance )
+    {
+    }
+    
+    
+}
+
+
+
+
+
+
+
