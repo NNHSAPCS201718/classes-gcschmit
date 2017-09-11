cc64f1de2aa9c52a65c713d5628666bb4767decc
Geoff Schmit <gcschmit@gmail.com>
2017-09-11 09:16:30 -0500

    started chapter 3 notes
    start with specifying the public interface

---
+++ Class Notes/Car.java
@@ -0,0 +1,30 @@
+
+/**
+ * This class models a car.
+ *
+ * @author gcschmit
+ * @version 11 September 2017
+ */
+public class Car
+{
+    /*
+     * Instance variables store the object's properties
+     *      specify the visibility (e.g., private)
+     *          public: accessible by anyone
+     *          private: only accessible by methods in this class
+     *      specify the type (e.g., double)
+     *      specify the name (e.g., fuelEfficiency)
+     *  
+     *  Instance variables differ from local variables in the following ways:
+     *      scoped to their object (accessible from all methods)
+     *      automatically initialized to a default value (0, false, null)
+     */
+    private double fuelEfficiency; // in units of miles per gallon
+    private double fuelInTank; // in units of gallons
+    
+    
+}
+
+
+
+
