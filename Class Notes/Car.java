0772b8167ee4a6744875a0d252af0a9ec78af456
Geoff Schmit <gcschmit@gmail.com>
2017-09-12 09:32:43 -0500

    stubbed the Car class
    generated the CarTest class; start with test methods next time

--- Class Notes/Car.java
+++ Class Notes/Car.java
@@ -22,7 +22,68 @@ public class Car
     private double fuelEfficiency; // in units of miles per gallon
     private double fuelInTank; // in units of gallons
     
+    /*
+     * Constructor:
+     *      responsible for intializing newly created objects
+     *      invoked automatically via the new operator
+     *      name of the constructor must match the name of the class
+     *      has no return type (not even void)
+     *      multiple constructors may be defined for a class
+     *      one constructor may call another constructor (with restrictions)
+     */
     
+    /**
+     * Default constructor for the Car class.
+     *  Sets the fuel efficiency to 30 mpg.
+     */
+    public Car()
+    {
+    }
+    
+    /**
+     * Constructs a Car object with the specified initial fuel efficiency.
+     * 
+     * @param initialEfficiency the initial fuel efficiency, in miles per gallon,
+     *                          of this new car
+     */
+    public Car( double initialEfficiency )
+    {
+    }
+    
+    /*
+     * When defining a method, specify:
+     *      the visibility (e.g., public)
+     *      the return type (e.g., void)
+     *      the method name (e.g., drive)
+     *      the parameters and their types (e.g., distance of type double)
+     */
+    /**
+     * Drives this car the specified distance, consuming fuel
+     * 
+     * @param distance the distance, in miles, this car drives
+     */
+    public void drive( double distance )
+    {
+    }
+    
+    /**
+     * Adds the specified amount of fuel to this car's tank
+     * 
+     * @param amount the amount of fuel, in gallons, to add to this car's tank
+     */
+    public void addGas( double amount )
+    {
+    }
+    
+    /**
+     * Returns the amount of fuel in this car's tank
+     * 
+     * @return the amount of fuel, in gallons, in this car's tank
+     */
+    public double getGasInTank()
+    {
+        return 0;
+    }
 }
 
 
