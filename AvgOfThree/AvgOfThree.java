0c1238f6f00e2188fa6a38dfcb65d3f83dda3e4d
Geoff Schmit <gcschmit@gmail.com>
2017-09-25 11:25:35 -0500

    finished Scanner example

--- AvgOfThree/AvgOfThree.java
+++ AvgOfThree/AvgOfThree.java
@@ -2,24 +2,82 @@ import java.util.Scanner;
 
 public class AvgOfThree
 {
-   public static void main (String[] args)
-   {
-       Scanner s = new Scanner(System.in);
-       double a, b, c;
+    public static void main (String[] args)
+    {
+        /*
+         * A scanner object parses primitive types and Strings from a stream.
+         * 
+         *  A stream is a sequence of characters from a file, String, keyboard,
+         *      network connection, etc.
+         *  
+         *  Parsing is separating a sequence of characters into token based on
+         *      deliminters.
+         *      A token is a meaningful sequence of characters (e.g., word).
+         *      Delimiters separate tokens (by default, whitespace (space, tab,
+         *          new line) is the delimiter).
+         *
+         *  When we create a Scanner object, we have to specify the input stream
+         *      (e.g., System.in which is the keyboard via the terminal window).
+         */
+        Scanner s = new Scanner(System.in);
+        double a, b, c;
 
-      System.out.print ("Enter the first number: ");
-      a = s.nextDouble();
+        /*
+         * Best practices:
+         *      1. prompt the user for what you want them to input
+         *      2. use print, not println, the the user input immediately follows the
+         *          colon
+         *      3. leave a space after the final colon
+         */
+        System.out.print ("Enter the first number: ");
+        
+        /*
+         * The nextDouble method attempts to convert the next token in the stream
+         *      to a double and return the value. If the next token cannot be
+         *      converted, an exception is generated.
+         *  
+         *  The nextInt method behaves in the same way.
+         */
+        a = s.nextDouble();
 
-      System.out.print ("Enter the second number: ");
-      b = s.nextDouble();
+        System.out.print ("Enter the second number: ");
+        b = s.nextDouble();
 
-      System.out.print ("Enter the third number: ");
-      c = s.nextDouble();
+        System.out.print ("Enter the third number: ");
+        c = s.nextDouble();
 
-      // compute the average of the three numbers
+        // compute the average of the three numbers
 
-      double avg = (a + b + c) / 3;
-      
-      System.out.println ("average " + avg);
-   }
+        double avg = (a + b + c) / 3;
+
+        System.out.println ("average " + avg);
+        
+        System.out.print( "Enter your first name: " );
+        
+        /*
+         * The next method returns the next token in the stream as a String
+         */
+        String firstName = s.next();
+        
+        /*
+         * Call the nextLine method in between calls to next and nextLine to
+         *      "consume" the the newline character "\n" in the stream.
+         */
+        s.nextLine();
+        
+        System.out.print( "Enter your street address: " );
+        
+        /*
+         * The nextLine method returns all characters up to the end of the line
+         *      (i.e., where the user typed enter)
+         */
+        String streetAddress = s.nextLine();
+        
+        System.out.println( "Your name: " + firstName + "\nYour address: " +
+                streetAddress );
+        
+        
+        
+        
+    }
 }
